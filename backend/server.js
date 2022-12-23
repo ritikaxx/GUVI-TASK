547b1467d4642b58b578f08547d58941a949b233
Ritika Singh <ritika.singh2019@vitstudent.ac.in>
2022-12-23 15:01:23 +0530

    completed

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,6 @@
 import express from "express";
 import dotenv from "dotenv";
 import connectDB from "./config/db.js";
-import colors from "colors";
 import path from "path";
 
 import noteRoutes from "./routes/noteRoutes.js";
@@ -22,17 +21,17 @@ app.use("/api/users", userRoutes);
 // --------------------------deployment------------------------------
 const __dirname = path.resolve();
 
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "/frontend/build")));
+// if (process.env.NODE_ENV === "production") {
+//   app.use(express.static(path.join(__dirname, "/frontend/build")));
 
-  app.get("*", (req, res) =>
-    res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"))
-  );
-} else {
+//   app.get("*", (req, res) =>
+//     res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"))
+//   );
+// } else {
   app.get("/", (req, res) => {
     res.send("API is running..");
   });
-}
+//}
 // --------------------------deployment------------------------------
 
 // Error Handling middlewares
